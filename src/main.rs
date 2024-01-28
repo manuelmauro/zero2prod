d47014325c9d54cbf255cdce75e63fac05917e26
Manuel Mauro <manuel.mauro@protonmail.com>
2024-01-28 22:14:17 +0100

    Add health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/health", get(health_check));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
 }
