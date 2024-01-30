2e2765322215ff264be372bf02f5f285993e70b6
Manuel Mauro <manuel.mauro@protonmail.com>
2024-01-30 17:22:16 +0100

    Adapt endpoint to book

--- src/app/subscription/mod.rs
+++ src/app/subscription/mod.rs
@@ -16,16 +16,16 @@ pub async fn subscribe(
     State(state): State<AppState>,
     Json(body): Json<NewSubscriber>,
 ) -> impl IntoResponse {
-    if let Ok(id) = sqlx::query_scalar!(
+    let record = sqlx::query!(
         r#"insert into subscriptions (id, email, name, subscribed_at) values ($1, $2, $3, $4) returning id"#,
         uuid::Uuid::new_v4(),
         body.email,
         body.name,
         chrono::Utc::now(),
+    ).fetch_one(&state.db).await;
 
-    ).fetch_one(&state.db).await {
-        return Ok(id.to_string());
-    };
-
-    Err(StatusCode::INTERNAL_SERVER_ERROR)
+    match record {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
 }
