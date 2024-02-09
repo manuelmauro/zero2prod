2ae07585953eb4fc9774bcd36e71838551b7f032
Manuel Mauro <manuel.mauro@protonmail.com>
2024-02-09 15:54:49 +0100

    Fill status column on subscriber registration

--- src/app/subscription/mod.rs
+++ src/app/subscription/mod.rs
@@ -34,11 +34,12 @@ async fn insert_subscriber(
     subscriber: NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        r#"insert into subscriptions (id, email, name, subscribed_at) values ($1, $2, $3, $4) returning id"#,
+        r#"insert into subscriptions (id, email, name, subscribed_at, status) values ($1, $2, $3, $4, $5) returning id"#,
         uuid::Uuid::new_v4(),
         subscriber.email.as_ref(),
         subscriber.name.as_ref(),
         chrono::Utc::now(),
+        "confirmed",
     ).fetch_one(db).await.map_err(|e| {
         tracing::error!(detail = e.to_string(), "failed to save new subscriber");
         e
