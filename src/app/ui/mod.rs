37bdda2c52a90adbb7be7b1dbe7ee2e34bed8e7f
Manuel Mauro <manuel.mauro@protonmail.com>
2024-03-06 10:57:21 +0100

    Rename home to admin

--- src/app/ui/mod.rs
+++ src/app/ui/mod.rs
@@ -2,10 +2,12 @@ use axum::Router;
 
 use super::AppState;
 
+mod admin;
 mod asset;
-mod home;
 mod login;
 
 pub fn router() -> Router<AppState> {
-    home::router().merge(login::router()).merge(asset::router())
+    admin::router()
+        .merge(login::router())
+        .merge(asset::router())
 }
