c1f5bc9820e5b476f0051749ad763ce040c0f06b
Manuel Mauro <manuel.mauro@protonmail.com>
2024-03-14 17:23:48 +0100

    Remove unwraps in session_user extractor

--- src/app/extractor/session_user.rs
+++ src/app/extractor/session_user.rs
@@ -1,11 +1,9 @@
-use axum::{
-    async_trait,
-    extract::FromRequestParts,
-    http::{self, request::Parts, StatusCode},
-};
+use axum::{async_trait, extract::FromRequestParts, http::request::Parts};
 use serde::{Deserialize, Serialize};
 use tower_sessions::Session;
 use uuid::Uuid;
+
+use crate::app::error::AppError;
 
 const USER_ID: &str = "user_id";
 
@@ -19,15 +17,18 @@ impl<S> FromRequestParts<S> for SessionUser
 where
     S: Send + Sync,
 {
-    type Rejection = (http::StatusCode, &'static str);
+    type Rejection = AppError;
 
     async fn from_request_parts(req: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let session = Session::from_request_parts(req, state).await?;
+        let session = Session::from_request_parts(req, state)
+            .await
+            .map_err(|e| AppError::Authorization(e.1.to_owned()))?;
         let user_id: Uuid = session
             .get(USER_ID)
             .await
-            .unwrap()
-            .ok_or((StatusCode::UNAUTHORIZED, "User not authenticated"))?;
+            .ok()
+            .flatten()
+            .ok_or_else(|| AppError::Authorization("User not in session".to_owned()))?;
 
         Ok(SessionUser { id: user_id })
     }
