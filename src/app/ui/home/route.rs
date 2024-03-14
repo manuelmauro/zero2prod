19cb0d3d8d99e6f3766c4fa045a942938dbf125e
Manuel Mauro <manuel.mauro@protonmail.com>
2024-03-14 17:27:19 +0100

    Remove unused counter in session

--- src/app/ui/home/route.rs
+++ src/app/ui/home/route.rs
@@ -1,12 +1,6 @@
 use askama::Template;
 use axum::response::IntoResponse;
-use serde::{Deserialize, Serialize};
 use tower_sessions::Session;
-
-const COUNTER_KEY: &str = "counter";
-
-#[derive(Default, Deserialize, Serialize)]
-struct Counter(usize);
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -14,9 +8,5 @@ struct HomeTemplate;
 
 #[tracing::instrument(name = "Home page")]
 pub async fn home_page(session: Session) -> impl IntoResponse {
-    let counter: Counter = session.get(COUNTER_KEY).await.unwrap().unwrap_or_default();
-    session.insert(COUNTER_KEY, counter.0 + 1).await.unwrap();
-    tracing::info!("Current count: {}", counter.0);
-
     HomeTemplate
 }
